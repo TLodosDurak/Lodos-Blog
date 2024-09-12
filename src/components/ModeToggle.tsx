d9545484187a85261b902a05ec943d92a2d1e84d
TLodosDurak <tlodosdurak@gmail.com>
2024-09-12 10:37:17 -0400

    Portfolio v1

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,26 +1,14 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
-
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { setTheme } = useTheme()
 
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
-  }
+  React.useEffect(() => {
+    setTheme('dark')
+  }, [setTheme])
 
-  return (
-    <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      {theme === 'light' ? (
-        <Sun className="h-[1.2rem] w-[1.2rem]" />
-      ) : (
-        <Moon className="h-[1.2rem] w-[1.2rem]" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
-  )
+  return null
 }
